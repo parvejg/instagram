3016cfcf1c93c9b780d9fd47c9c33cd4b494bd16
parvejg <parvezpg321@gmail.com>
2023-12-16 23:10:08 +0530

    add img and sign-in page strc

--- src/Components/SignIn-page.js
+++ src/Components/SignIn-page.js
@@ -1,4 +1,17 @@
 import "./SignInPage.css";
 export const SignInPage = () => {
-  return <div className="signIn-page-wrapper"></div>;
+  return (
+    <div className="signIn-page-wrapper">
+      <div className="signInpage-img-wrapper">
+        <img src="https://static.toiimg.com/thumb/resizemode-4,msid-92982598,imgsize-14370,width-720/92982598.jpg" />
+      </div>
+      <div className="sign-in-page-wrapper">
+        <h1>Instagram</h1>
+        <input placeholder="phone number, email user name" type="text" />
+        <input placeholder="password" type="password" />
+        <button>Log in</button>
+        <p>-------or--------</p>
+      </div>
+    </div>
+  );
 };
