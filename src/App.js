99bef8a1bbe968074b0d35f4e434c352a77353d4
parvejg <parvezpg321@gmail.com>
2023-12-21 21:09:40 +0530

    improvement

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import { SignInPage } from "./Components/SignIn-page";
+import { SignUpPage } from "./Components/SignUp-page";
 
 function App() {
   return (
     <div className="App">
       <SignInPage />
+      {/* <SignUpPage /> */}
     </div>
   );
 }
