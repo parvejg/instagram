bd2f273abc44781ed0ae281fdb3a2bf8be740ff0
parvejg <parvezpg321@gmail.com>
2023-12-21 21:19:11 +0530

    add a reusable function

---
+++ src/Components/ReusableFunction.js
@@ -0,0 +1,9 @@
+export const BasicInput = (placeholder, className) => {
+  return (
+    <input
+      className="sign-in-email-input"
+      placeholder="Phone number, email user name"
+      type="text"
+    />
+  );
+};
