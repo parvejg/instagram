5153d546950921d96d7aca7a28022a3f67996b98
parvejg <parvezpg321@gmail.com>
2023-12-25 00:54:13 +0530

    add a contents file or component or css file

---
+++ src/Components/Contents.js
@@ -0,0 +1,8 @@
+import "./Contents.css";
+export const Contents = () => {
+  return (
+    <div className="contents-main-wrapper">
+      <div className="contents-wrapper"></div>
+    </div>
+  );
+};
