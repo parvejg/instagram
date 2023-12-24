4e271cc5b9ae34699b34aa8740eba6f89d7f7043
parvejg <parvezpg321@gmail.com>
2023-12-24 23:32:03 +0530

    add links in  r sidebar

--- src/Components/Landing-page.js
+++ src/Components/Landing-page.js
@@ -4,7 +4,33 @@ export const LandingPage = () => {
   return (
     <div className="landing-page-container">
       <div className="landing-page-left-sidebar-wrapper">
-        <InstagramTextImage className="sidebar-text-img" />
+        <div>
+          <InstagramTextImage className="sidebar-text-img" />
+        </div>
+        <a href="#" className="r-sidebar-links">
+          Home
+        </a>
+        <a href="#" className="r-sidebar-links">
+          Search
+        </a>
+        <a href="#" className="r-sidebar-links">
+          Explore
+        </a>
+        <a href="#" className="r-sidebar-links">
+          Reels
+        </a>
+        <a href="#" className="r-sidebar-links">
+          Messages
+        </a>
+        <a href="#" className="r-sidebar-links">
+          Notifications
+        </a>
+        <a href="#" className="r-sidebar-links">
+          Create
+        </a>
+        <a href="#" className="r-sidebar-links">
+          Profile
+        </a>
       </div>
       <div className="landing-page-content-wrapper"></div>
       <div className="landing-page-right-sidebar-wrapper"></div>
