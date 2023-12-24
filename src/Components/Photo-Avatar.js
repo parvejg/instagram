1bfdd59626de4e7a459b5e88e9973dae52856113
parvejg <parvezpg321@gmail.com>
2023-12-25 01:46:54 +0530

    improvement

--- src/Components/Photo-Avatar.js
+++ src/Components/Photo-Avatar.js
@@ -3,7 +3,7 @@ export const PhotoAvatar = ({ className, src, userName }) => {
   return (
     <>
       <img alt="avatar-img" src={src} className={className} />
-      <small>{userName}</small>
+      <small className="content-avatar-userName">{userName}</small>
     </>
   );
 };
