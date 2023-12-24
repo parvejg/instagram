7c7bdde1107a6839020fb8589f8d131ae7932abf
parvejg <parvezpg321@gmail.com>
2023-12-25 00:03:18 +0530

    add sidebar link icons

--- src/Components/Landing-page.js
+++ src/Components/Landing-page.js
@@ -1,5 +1,12 @@
 import "./Landing-page.css";
+import { AiFillHome } from "react-icons/ai";
+import { IoSearch } from "react-icons/io5";
 import { InstagramTextImage } from "./ReusableComponents/InstagramText-img";
+import { FcFilmReel } from "react-icons/fc";
+import { MdExplore } from "react-icons/md";
+import { AiOutlineMessage } from "react-icons/ai";
+import { IoIosHeartEmpty } from "react-icons/io";
+import { CiCirclePlus } from "react-icons/ci";
 export const LandingPage = () => {
   return (
     <div className="landing-page-container">
@@ -8,25 +15,26 @@ export const LandingPage = () => {
           <InstagramTextImage className="sidebar-text-img" />
         </div>
         <a href="#" className="r-sidebar-links">
-          Home
+          <AiFillHome className="sidebar-icon" /> Home
         </a>
         <a href="#" className="r-sidebar-links">
+          <IoSearch className="sidebar-icon" />
           Search
         </a>
         <a href="#" className="r-sidebar-links">
-          Explore
+          <MdExplore className="sidebar-icon" /> Explore
         </a>
         <a href="#" className="r-sidebar-links">
-          Reels
+          <FcFilmReel className="sidebar-icon" /> Reels
         </a>
         <a href="#" className="r-sidebar-links">
-          Messages
+          <AiOutlineMessage className="sidebar-icon" /> Messages
         </a>
         <a href="#" className="r-sidebar-links">
-          Notifications
+          <IoIosHeartEmpty className="sidebar-icon" /> Notifications
         </a>
         <a href="#" className="r-sidebar-links">
-          Create
+          <CiCirclePlus className="sidebar-icon" /> Create
         </a>
         <a href="#" className="r-sidebar-links">
           Profile
