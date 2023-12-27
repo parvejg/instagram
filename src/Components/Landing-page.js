b14a8974091c1ec9468987ccebffe255047393bb
parvejg <parvezpg321@gmail.com>
2023-12-27 23:37:04 +0530

    complete search-page

--- src/Components/Landing-page.js
+++ src/Components/Landing-page.js
@@ -6,10 +6,12 @@ import { LeftSidebar } from "./Left-sidebar";
 import { Avatar } from "./Avatar";
 import { Contents } from "./Contents";
 import { RightSidebar } from "./Right-sidebar";
+import { SearchPage } from "./Search-Page";
 export const LandingPage = () => {
   return (
     <div className="landing-page-container">
-      <LeftSidebar />
+      {/* <LeftSidebar /> */}
+      <SearchPage />
       <Contents />
       <RightSidebar />
     </div>
