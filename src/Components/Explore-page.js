fa2d75c26a3fc1a5a9188ee927c0eec0566f8642
parvejg <parvezpg321@gmail.com>
2023-12-27 23:39:07 +0530

    added an explore page

---
+++ src/Components/Explore-page.js
@@ -0,0 +1,8 @@
+import "./Explore-page.css";
+export const ExplorePage = () => {
+  return (
+    <div className="explore-page-wrapper">
+      <div className="explore-page"></div>
+    </div>
+  );
+};
