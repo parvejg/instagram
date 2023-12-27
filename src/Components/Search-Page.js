df25e06a24e914f5717c87346ba828522de32439
parvejg <parvezpg321@gmail.com>
2023-12-27 22:32:53 +0530

    add a searchpage or css file

---
+++ src/Components/Search-Page.js
@@ -0,0 +1,8 @@
+import "./Search-page.css";
+export const SearchPage = () => {
+  return (
+    <div className="search-page-wrapper">
+      <div className="search-page"></div>
+    </div>
+  );
+};
