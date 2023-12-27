2bc62b16c335b75574a59c96cb5ba4fee5089e1c
parvejg <parvezpg321@gmail.com>
2023-12-27 22:08:33 +0530

    improvement

--- src/Components/ReusableComponents/Sidebar-link.js
+++ src/Components/ReusableComponents/Sidebar-link.js
@@ -3,7 +3,7 @@ export const SidebarLink = ({ icon, linkName, href }) => {
   return (
     <div className="left-sidebar-wrapper">
       <a href={href} className="l-sidebar-links">
-        {icon} {linkName}
+        <span> {icon}</span> <span> {linkName}</span>
       </a>
     </div>
   );
--- src/Components/Right-sidebar.js
+++ src/Components/Right-sidebar.js
@@ -1,6 +1,50 @@
-import { Avatar } from "./Avatar";
 import { FollowBtn } from "./Follow-Btn";
+import { ProfileAvatar } from "./ReusableComponents/Profile-avatar";
 import "./Right-sidebar.css";
 export const RightSidebar = () => {
-  return <div className="right-sidebar-main-wrapper"></div>;
+  return (
+    <div className="right-sidebar-main-wrapper">
+      <ProfileAvatar
+        userName="parvez321pg"
+        name="MD Parvez"
+        button={<FollowBtn btnName="Switch" />}
+      />
+      <div className="see-all-btn-wrapper">
+        <small className="suggested-for-you-text">Suggested for you</small>
+        <button className="see-all-btn">See All</button>
+      </div>
+      <div className="right-sidebar-users-wrapper">
+        <ProfileAvatar
+          userName="Aashu"
+          name="follow you"
+          button={<FollowBtn btnName="Follow" />}
+        />
+        <ProfileAvatar
+          userName="Rahul_Choudhary"
+          name="follow you"
+          button={<FollowBtn btnName="Follow" />}
+        />
+        <ProfileAvatar
+          userName="Mohit_sharma"
+          name="follow you"
+          button={<FollowBtn btnName="Follow" />}
+        />
+        <ProfileAvatar
+          userName="Guddu"
+          name="New to Instagram"
+          button={<FollowBtn btnName="Follow" />}
+        />
+        <ProfileAvatar
+          userName="Rohit"
+          name="New to Instagram"
+          button={<FollowBtn btnName="Follow" />}
+        />
+        <ProfileAvatar
+          userName="Ubaid_pathan"
+          name="New to Instagram"
+          button={<FollowBtn btnName="Follow" />}
+        />
+      </div>
+    </div>
+  );
 };
