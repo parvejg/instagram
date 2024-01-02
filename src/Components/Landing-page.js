73390a117fbb1efb7855858b09b90d79b2e7802a
parvejg <parvezpg321@gmail.com>
2024-01-02 22:24:29 +0530

    explore imgCard improvement

--- src/Components/Landing-page.js
+++ src/Components/Landing-page.js
@@ -2,12 +2,6 @@ import "./Landing-page.css";
 import "./Avatar.css";
 import "./Left-sidebar.css";
 
-import { LeftIconSidebar, LeftSidebar } from "./Left-sidebar";
-import { Avatar } from "./Avatar";
-import { Contents } from "./Contents";
-import { RightSidebar } from "./Right-sidebar";
-import { SearchPage } from "./Search-Page";
-import { ExplorePage } from "./Explore-page";
 import { ExploreImageCard } from "./ExploreImg-card";
 export const LandingPage = () => {
   return (
