9e5a548b24467a1f38c3f51f4340eb8eaccb2c08
parvejg <parvezpg321@gmail.com>
2024-01-04 19:54:03 +0530

    improvement

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { LandingPage } from "./Components/Landing-page";
+import { SearchPage } from "./Components/Search-Page";
+import { ExplorePage } from "./Components/Explore-page";
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
           <Route path="/" Component={LandingPage} />
+          <Route path="landing-page" Component={LandingPage} />
+          <Route path="search-page" Component={SearchPage} />
+          <Route path="explore-page" Component={ExplorePage} />
         </Routes>
       </Router>
     </div>
