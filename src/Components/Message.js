394a24b656e3002a7c6b3e48f5ee26c58b851016
parvejg <parvezpg321@gmail.com>
2024-01-04 23:26:25 +0530

    improve links css

--- src/Components/Message.js
+++ src/Components/Message.js
@@ -7,15 +7,21 @@ export const MessagePage = () => {
       <div className="left-iconSidebar-wrapper">
         <LeftIconSidebar />
       </div>
-      <div>
+      <div className="userName-and-links-main-wrapper">
         <div className="userName-wrapper">
           <h2>Parvezpg321</h2>
           <FiEdit2 className="edit-icon" />
         </div>
         <div className="message-links-wrapper">
-          <a href="/">Primary</a>
-          <a href="/">General</a>
-          <a href="/">Request</a>
+          <a href="/" className="message-link">
+            Primary
+          </a>
+          <a href="/" className="message-link">
+            General
+          </a>
+          <a href="/" className="message-link">
+            Request
+          </a>
         </div>
       </div>
     </div>
