c539f8208bc81b9deaa64179399eeeb657dd0d40
parvejg <parvezpg321@gmail.com>
2024-01-10 22:10:31 +0530

    added more options btn page

--- src/Components/MoreOptions-page.js
+++ src/Components/MoreOptions-page.js
@@ -1,15 +1,19 @@
 import "./MoreOptions-page.css"
 import { SidebarLink } from "./ReusableComponents/Sidebar-link"
+import { IoSettingsOutline } from "react-icons/io5";
+import { LuActivitySquare,LuBookmark } from "react-icons/lu";
+import { MdOutlineLightMode } from "react-icons/md";
+import { TbMessageReport,TbBrandThreads } from "react-icons/tb";
 export const MoreOptions=() =>{
     return(
         <div className="more-options-page-wrapper">
             <div className="moreOptions-page-links-wrapper">
-                <SidebarLink linkName="Setting"/>
-                <SidebarLink linkName="Your activity"/>
-                <SidebarLink linkName="Saved"/>
-                <SidebarLink linkName="Switch appearance"/>
-                <SidebarLink linkName="Report a problem"/>
-                <SidebarLink linkName="Threads"/>
+ <SidebarLink icon={<IoSettingsOutline className="more-option-icon"/>}   linkName="Setting" />
+                <SidebarLink icon={<LuActivitySquare className="more-option-icon"/>} linkName="Your activity"/>
+                <SidebarLink icon={<LuBookmark className="more-option-icon"/>} linkName="Saved"/>
+                <SidebarLink icon={< MdOutlineLightMode  className="more-option-icon" />} linkName="Switch appearance"/>
+                <SidebarLink  icon={<TbMessageReport className="more-option-icon" />}linkName="Report a problem"/>
+                <SidebarLink icon={<TbBrandThreads className="more-option-icon" />} linkName="Threads"/>
                 <SidebarLink linkName="Switch accounts"/>
                 <SidebarLink linkName="Log out"/>
             </div>
